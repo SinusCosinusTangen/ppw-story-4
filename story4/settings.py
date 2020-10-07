1ad8dc2b236bb36e375d3a420976a477d0c80b0f
James Frederix Rolianto <jamesfrederix16@gmail.com>
2020-10-07 20:11:39 +0700

    hmm

--- story4/settings.py
+++ story4/settings.py
@@ -130,3 +130,7 @@ STATIC_URL = '/static/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "story3/static",
+]
