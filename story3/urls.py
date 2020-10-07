d0c14674da2bc7474bdf24b01264f6a0bd477da8
James Frederix Rolianto <jamesfrederix16@gmail.com>
2020-10-07 19:36:57 +0700

    aaaaaaaaaaaaaaaaaaaaaaa

--- story3/urls.py
+++ story3/urls.py
@@ -6,8 +6,8 @@ app_name = 'story3'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/interest.html', views.index, name='interest'),
-    path('/aboutme.html', views.index, name='aboutme'),
-    path('/gallery.html', views.index, name='gallery'),
-    path('/credits.html', views.index, name='credits'),
+    path('/interest.html/', views.index, name='interest'),
+    path('/aboutme.html/', views.index, name='aboutme'),
+    path('/gallery.html/', views.index, name='gallery'),
+    path('/credits.html/', views.index, name='credits'),
 ]
