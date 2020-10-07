998e004838ca700e8ee871456393fbdd50f3409a
James Frederix Rolianto <jamesfrederix16@gmail.com>
2020-10-07 20:02:36 +0700

    dalam nama Tuhan Yesus story 4 akan kelar

--- story4/settings.py
+++ story4/settings.py
@@ -130,8 +130,3 @@ STATIC_URL = '/static/'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    'var/www/static/',
-]
