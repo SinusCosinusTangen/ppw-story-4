49926e2fe9f2f3237b6e58651eadf1bd55219234
James Frederix Rolianto <jamesfrederix16@gmail.com>
2020-10-07 19:51:46 +0700

    dalam nama Yesus

--- story3/urls.py
+++ story3/urls.py
@@ -6,8 +6,8 @@ app_name = 'story3'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/interest.html/', views.index, name='interest'),
-    path('/aboutme.html/', views.index, name='aboutme'),
-    path('/gallery.html/', views.index, name='gallery'),
-    path('/credits.html/', views.index, name='credits'),
+    path('/interest.html', views.interest, name='interest'),
+    path('/aboutme.html', views.aboutme, name='aboutme'),
+    path('/gallery.html', views.gallery, name='gallery'),
+    path('/credits.html', views.credits, name='credits'),
 ]
